9eeaa0b0508007fdbe10d0f1b496635c590942b5
Adriana Amelia Troncoso Guerrero <AdrianaTroncoso-CloudUp>
2021-05-28 22:19:03 -0500

    PAgination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Posts from './components/Posts';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const[currentPage, setCurrentPage] = useState(1);
+  const [postPerPage,setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  },[]);
+
+  //Get current posts
+    const indexOfLastPost = currentPage *postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+
+    const columns = [
+      { field: 'posts', headerName: 'Posts', width: 270 }
+    ];
+  
+    const rows = [
+      {posts: currentPosts}
+    ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={currentPosts} loading={loading}/>
     </div>
+
   );
 }
 
